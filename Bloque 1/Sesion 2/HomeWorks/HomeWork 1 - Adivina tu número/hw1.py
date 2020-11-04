bc05c65d2736062820404ae3997216c54954bacd
JuanLuisCorredor <55844039+JuanLuisCorredor@users.noreply.github.com>
2020-11-04 13:24:55 +0100

    Update hw1.py
    asd

--- Bloque 1/Sesion 2/HomeWorks/HomeWork 1 - Adivina tu número/hw1.py
+++ Bloque 1/Sesion 2/HomeWorks/HomeWork 1 - Adivina tu número/hw1.py
@@ -3,23 +3,23 @@
 #this code is not working well
 # correct the "print" to be correct
 
-print("Piense un número de 1 a 4.")
-print("Conteste S (sí) o N (no) a mis preguntas.")
+print("Piense un numero de 1 a 4.")
+print("Conteste S (si) o N (no) a mis preguntas.")
 primera = input("¿El número pensado es mayor que 2? ")
 if primera == "S":
     segunda = input("¿El número pensado es mayor que 3? ")
     if segunda == "S":
         #print 1
-        print("El número pensado es 1.")
+        print("El numero pensado es 1.")
     else:
         #print 2
-        print("El número pensado es 2")
+        print("El numero pensado es 2")
 else:
-    segunda = input("¿El número pensado es mayor que 1? ")
+    segunda = input("¿El numero pensado es mayor que 1? ")
     if segunda != "S":
         #print 3
-        print("El número pensado es 4.")
+        print("El numero pensado es 4.")
     else:
         #print 4
-        print("El número pensado es 3.")
-print("¡Hasta la próxima!")
+        print("El numero pensado es 3.")
+print("¡Hasta la proxima!")
