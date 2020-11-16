a9121f43838b71b27eaf21ab2ffb2591cde8aca7
JuanLuisCorredor <55844039+JuanLuisCorredor@users.noreply.github.com>
2020-11-16 13:02:28 +0100

    cambios examen
    cambios examen

--- Bloque 1/Examen/bucle_condicional.py
+++ Bloque 1/Examen/bucle_condicional.py
@@ -1,12 +1,13 @@
 # Crea un bucle que recorra todos los elementos de la sieguiente lista de importes, en cada iteración se debe ejecutar el cuerpo 1 o el cuerpo 2. 
-# El "cuerpo 1" se ejecutara si el importe del elemento que estamos iterando, es mayor que la raiz cuadrada de la variable "importe_referencia" en caso contrario se ejecutara el "cuerpo 2". 
+# El "cuerpo 1" se ejecutara si el importe del elemento que estamos iterando, es menor que la raiz cuadrada de la variable "importe_referencia" en caso contrario se ejecutara el "cuerpo 2". 
+# Create a loop that goes through all the elements of the following list of amounts, in each iteration body 1 or body 2 must be executed.
+# The "body 1" will be executed if the amount of the element that we are iterating, is less than the square root of the variable "amount_reference", otherwise "body 2" will be executed.
 
-### NO TOCAR INICIO
+### NO TOCAR DATOS INICIALES 
+### DO NOT INICIAL DATA
 resultado= 0
 lista_importes = [10, 25, 13, 15, 17, 17, 11]
 importe_referencia = 144
-
-### NO TOCAR File
 
 
 #Escribe tu código a partir de aqui:
@@ -21,13 +22,15 @@ importe_referencia = 144
 
 
 ##### INTRODUCIR EN EL FORMULARIO DEL EXAMEN EL VALOR QUE APARECE AL EJECUTAR 
+##### ENTER IN THE EXAM FORM THE VALUE THAT APPEARS WHEN EXECUTING
 ##### SOLO CON 3 DIGITOS DECIMALES!!!!!
-#####
-##### Ejemplo:
+##### ONLY WITH 3 DECIMAL DIGITS !!!!!
+##### Ejemplo/Example:
 #####
 ##### RESULTADO -> 2.21535184  VALOR A INTRODUCIR EN EL FORMULARIO DEL EXAMEN -> 2.215
-#####
+##### RESULT -> 2.21535184 VALUE TO ENTER IN THE EXAM FORM -> 2.215
 #####
 
 #Imprime la variable resultado, para rellenar en forms
+#Print the result variable, to fill in forms
 print(resultado)
